fb08af592e311275b0c4ba75cb170eb7c9482833
Lucas-Dorchy <lucas.dorchy@o2do.be>
2025-01-28 13:45:21 +0100

    Update Program.cs

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -3,4 +3,8 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello AutoDo, Eric is the best !");
 
+app.MapGet("/generate-error", () =>
+{
+    throw new Exception("Error : test log !");
+});
 app.Run();
