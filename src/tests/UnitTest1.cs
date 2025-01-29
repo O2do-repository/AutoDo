5d13237886fa04d404280482c1af0b79e51eca31
Lucas-Dorchy <lucas.dorchy@o2do.be>
2025-01-29 15:15:29 +0100

    testing

---
+++ src/tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace tests;
+
+using Xunit;
+
+namespace Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void Addition_SimpleValues_ReturnsCorrectSum()
+        {
+            int a = 2;
+            int b = 3;
+            int result = a + b;
+
+            Assert.Equal(5, result);
+        }
+    }
+}
