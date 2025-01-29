0cba5fbbf08388e61542090ebb0d95bdda5b2095
Lucas-Dorchy <lucas.dorchy@o2do.be>
2025-01-29 15:59:34 +0000

    failed testing

--- src/tests/UnitTest1.cs
+++ src/tests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace Tests
         public void Addition_SimpleValues_ReturnsCorrectSum()
         {
             int a = 2;
-            int b = 3;
+            int b = 4;
             int result = a + b;
 
             Assert.Equal(5, result);
