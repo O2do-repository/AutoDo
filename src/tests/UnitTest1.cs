5814a4f47597d00b45ed586a10bb21434fbfa834
Lucas-Dorchy <lucas.dorchy@o2do.be>
2025-01-30 10:20:33 +0100

    Update UnitTest1.cs

--- src/tests/UnitTest1.cs
+++ src/tests/UnitTest1.cs
@@ -2,18 +2,17 @@
 
 using Xunit;
 
-namespace Tests
-{
-    public class SampleTests
-    {
-        [Fact]
-        public void Addition_SimpleValues_ReturnsCorrectSum()
-        {
-            int a = 2;
-            int b = 4;
-            int result = a + b;
 
-            Assert.Equal(5, result);
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Addition_SimpleValues_ReturnsCorrectSum()
+    {
+        int a = 2;
+        int b = 4;
+        int result = a + b;
+
+        Assert.Equal(5, result);
     }
 }
+
