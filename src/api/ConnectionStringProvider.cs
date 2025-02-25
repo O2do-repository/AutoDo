fba6a02ef9da99460a90dcccd39cf4bc1775aaec
Lucas-Dorchy <lucas.dorchy@o2do.be>
2025-02-25 13:40:59 +0100

    test prod db

--- src/api/ConnectionStringProvider.cs
+++ src/api/ConnectionStringProvider.cs
@@ -10,6 +10,17 @@ public class ConnectionStringProvider : IConnectionStringProvider{
 
     public string Get(string key)
     {
+        // Détection simple d'Azure
+        bool isAzure = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME"));
+        
+        if (isAzure)
+        {
+            string dataPath = Path.Combine(Environment.GetEnvironmentVariable("HOME"), "data");
+            Directory.CreateDirectory(dataPath); // Crée le dossier s'il n'existe pas
+            return $"Data Source={Path.Combine(dataPath, "AutoDo.db")}";
+        }
+        
+        // En local, utiliser la configuration
         return _configuration.GetConnectionString(key);
     }
 }
